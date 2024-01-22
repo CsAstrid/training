140dccb4c1703e79ff4af1459bd23e2ab97d9b5e
CsAstrid <79113694+CsAstrid@users.noreply.github.com>
2024-01-22 08:38:45 +0100

    elso modositott

--- elso.java
+++ elso.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Elso vagyok");
+        System.out.println("Elso vagyok, de mar modositott!");
     }
-}
+}git 
